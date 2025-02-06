0f6dddab7cc4d896fd89dd285969ac08e1e4780c
manideep-yellani <manideep577@gmail.com>
2025-02-06 10:24:31 -0500

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
 
 function App() {
   return (
